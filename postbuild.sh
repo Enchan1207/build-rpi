0f48d0bf2907c697b9710716c1dd421cbb49d3f3
Enchan <enchant0130@gmail.com>
2023-10-29 12:08:58 +0900

    [Add] script files

---
+++ postbuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Buildroot post-build script
+#
+
+# キャッシュを軽量化するため、不要なファイルを削除する
+ls -1a /dist/build | sed "/^host-/d" | sed "/^\./d" | xargs -i rm -rf "/dist/build/{}"
+rm -rf /dist/images /dist/target
---
+++ prebuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Buildroot pre-build script
+#
+cd /buildroot
+
+# Pythonパッケージの追加
+yes | ./utils/scanpypi psycopg2 usb python-usbtmc sqlalchemy
