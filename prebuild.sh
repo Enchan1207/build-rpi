08ed21645982f671e6f1ccbefc6b63b0c35d9549
Enchan <enchant0130@gmail.com>
2023-11-02 17:04:21 +0900

    [Modify] config, pre-build script

--- prebuild.sh
+++ prebuild.sh
@@ -3,9 +3,3 @@
 # Buildroot pre-build script
 #
 cd /buildroot
-
-# Pythonパッケージの追加
-yes | ./utils/scanpypi usb python-usbtmc
-
-# 強制的にglibc utilを埋め込む
-echo "BR2_PACKAGE_GLIBC_UTILS=y" >> /dist/.config
